a98c2726b70b1eea72773188658c3ca117c5590b
devidrees <dev.idrees@gmail.com>
2026-03-31 14:20:53 +0530

    first commit

---
+++ Day 02 - Environments and Conditionals/app.py
@@ -0,0 +1,12 @@
+x = int(input("Enter #1: "))
+y = int(input("Enter #2: "))
+ 
+print("Sum: ", x + y)
+print("Diff: ", x - y)
+print("Mul: ", x * y)
+print("Div: ", x / y)
+
+print("Floor Div: ", x // y)
+print("Modulus: ", x % y)
+
+print("Exponent: ", x ** y)
